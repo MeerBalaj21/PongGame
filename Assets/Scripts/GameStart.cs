0f458d94cdc51ff3d9a40009119343925b3e1c40
Meer.Balaj <meer.balaj@mindstudios.com>
2022-08-15 00:51:16 +0500

    implemented gamestart

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class GameStart : MonoBehaviour
 {
+    public GameObject Ball;
+    public GameObject StartButton;
+    public GameObject Paddles;
+    public GameObject Scores;
+    public GameObject Win;
+    public GameObject Lose;
+
     public void StartGame()
     {
-        gameObject.SetActive(false);
+        StartButton.gameObject.SetActive(false);
+        Scores.gameObject.SetActive(true);
+        Ball.gameObject.SetActive(true);
+        Paddles.gameObject.SetActive(true);
+        Lose.gameObject.SetActive(false);
+        Win.gameObject.SetActive(false);
+    }
+
+    public void ResetGame()
+    {
+        StartButton.gameObject.SetActive(true);
+        Scores.SetActive(false);
+        Ball.gameObject.SetActive(false);
+        Paddles.gameObject.SetActive(false);
+        
     }
 }
