80aa246d7ed3eb152cb6b053018b7ce3a459cf9a
Meer.Balaj <meer.balaj@mindstudios.com>
2022-08-15 00:52:08 +0500

    implemented scene builder

--- Assets/Scripts/SceneBuilder.cs
+++ Assets/Scripts/SceneBuilder.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class SceneBuilder : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float _width;
+    public static float _width;
     public GameObject WallLeft;
     public GameObject WallRight;
     public GameObject WallTop;
     public GameObject WallBottom;
+    public GameObject PaddleTop;
+    public GameObject PaddleBottom;
 
     void Start()
     {
         WidthCalculator();
         WallPlacer();
         WallScaler();
+        PaddlePlacer();
+        PaddleScaler();
     }
 
     private void WidthCalculator()
@@ -29,15 +33,25 @@ public class SceneBuilder : MonoBehaviour
         WallRight.transform.position = new Vector2(_width/2, 0);
         WallTop.transform.position = new Vector2(0, 5);
         WallBottom.transform.position = new Vector2(0, -5);
-
     }
 
     private void WallScaler()
     {
-        WallLeft.transform.localScale = new Vector2(0.5f,10);
+        WallLeft.transform.localScale = new Vector2(0.5f, 10);
         WallRight.transform.localScale = new Vector2(0.5f, 10);
-        WallTop.transform.localScale = new Vector2(_width, 0.5f);
-        WallBottom.transform.localScale = new Vector2(_width,0.5f);
+        WallTop.transform.localScale = new Vector2(_width, 0.1f);
+        WallBottom.transform.localScale = new Vector2(_width, 0.1f);
     }
 
+    private void PaddlePlacer()
+    {
+        PaddleTop.transform.position = new Vector2(0, 4.90f);
+        PaddleBottom.transform.position = new Vector2(0, -4.90f);
+    }
+
+    private void PaddleScaler()
+    {
+        PaddleTop.transform.localScale = new Vector2(_width/3,0.25f);
+        PaddleBottom.transform.localScale = new Vector2(_width /3, 0.25f);
+    }
 }
