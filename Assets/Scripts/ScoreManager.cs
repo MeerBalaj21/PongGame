b747b4ab83b3dd1b4aa9828a68e2824944e29d2e
Meer.Balaj <meer.balaj@mindstudios.com>
2022-08-15 00:50:45 +0500

    implemented score manager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class ScoreManager : GameStart
+{
+    public static UnityEvent GoalTop;
+    public static UnityEvent GoalBottom;
+    public GameObject PlayerWin;
+    public GameObject PlayerLose;
+    public TMP_Text PlayerWinText;
+    public TMP_Text PlayerLoseText;
+
+    public int ScorePlayer;
+    public int ScoreEnemy;
+
+    private void Start()
+    {
+        PlayerLose.SetActive(false);
+        ScorePlayer = 0;
+        GoalTop = new UnityEvent();
+        GoalTop.AddListener(ScoreTop);
+
+        PlayerWin.SetActive(false);
+        ScoreEnemy = 0;
+        GoalBottom = new UnityEvent();
+        GoalBottom.AddListener(ScoreBottom);
+    }
+
+    private void ScoreTop()
+    {
+        ScorePlayer = ScorePlayer + 1; 
+    }
+
+    private void ScoreBottom()
+    {
+        ScoreEnemy = ScoreEnemy + 1;
+    }
+
+    private void ResetScore()
+    {
+        ScoreEnemy = 0;
+        ScorePlayer = 0;
+    }
+
+    private void Update()
+    {
+        PlayerLoseText.text = ScoreEnemy.ToString();
+        PlayerWinText.text = ScorePlayer.ToString();
+
+        if (ScoreEnemy == 3)
+        {
+            PlayerLose.SetActive(true);
+            ResetScore();
+            ResetGame();
+        }
+        if (ScorePlayer == 3)
+        {
+            PlayerWin.SetActive(true);
+            ResetScore();
+            ResetGame();
+        }
+
+    }
+}
